03ef123197b485c106beb32d3178a51cf803111b
Dave T <105549233+Dmum303@users.noreply.github.com>
2022-12-27 11:43:52 +0100

    session route made

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "sessions#home"
+
+  # User routes
+  resources :users, only: %i[new create edit update show destroy]
+
+  #Sessions routes
+  get "/login", to: "sessions#login"
+  post "/login", to: "sessions#create"
+  post "/logout", to: "sessions#destroy"
+  get "/logout", to: "sessions#destroy"
 end
