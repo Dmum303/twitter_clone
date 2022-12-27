635312d15f938172dc75a5b843cd376d355abff6
Dave T <105549233+Dmum303@users.noreply.github.com>
2022-12-27 17:30:48 +0100

    signup actioned

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "User created."
+      puts "User created."
       redirect_to root_path
     else
       render "new"
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # root
+  root "posts#index"
 
+  get "posts", to: "posts#index"
   get "signup", to: "users#new"
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
