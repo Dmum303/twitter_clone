0206e988ee81d41bca072266147e1ad84c6fc74f
Dave T <105549233+Dmum303@users.noreply.github.com>
2022-12-27 18:24:07 +0100

    Sessions login and logout completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,13 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def edit
+    # method inherited from app_con
+    if !logged_in?
+      #redirect to standard index
+    else
+      #do cool stuff
+    end
+  end
 end
