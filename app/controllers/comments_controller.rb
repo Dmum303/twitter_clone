0f80b4fcaa64463bde362745627d26390d0b027f
Dave T <105549233+Dmum303@users.noreply.github.com>
2023-01-04 15:37:41 +0000

    Cleaning up comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,12 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    params[:user_id] = "6"
     puts "this is here"
-    # puts session[:user_id].to_s
     amended_params = comment_params
     amended_params[:user_id] = session[:user_id]
     amended_params[:post_id] = params[:post_id]
-    puts amended_params
     @comment = @post.comments.create(amended_params)
 
-    # below looks wrong, is it calling the the create method on the post class?
-    # or is it creating a comment in the post colum of db with comment params?
-    #  would like to look at whats inside @comment
-    # @comment[:user_id] = session[:user_id]
-    # @comment[:post_id] = params[:post_id] - post id is in params already!
     redirect_to post_path(@post)
   end
 
