2a3706d207702ccbc7ece182adfd8ac8bd91fb44
Dave T <105549233+Dmum303@users.noreply.github.com>
2023-01-07 16:49:56 +0000

    Stuck on getting svg icon to display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,20 @@ module ApplicationHelper
   def current_user
     @current_user ||= User.find_by_id(session[:user_id]) if !!session[:user_id]
   end
+
+  def embedded_svg(filename, options = {})
+    assets = Rails.application.assets
+    asset = assets.find_asset(filename)
+
+    if asset
+      file = asset.source.force_encoding("UTF-8")
+      doc = Nokogiri::HTML::DocumentFragment.parse file
+      svg = doc.at_css "svg"
+      svg["class"] = options[:class] if options[:class].present?
+    else
+      doc = "<!-- SVG #{filename} not found -->"
+    end
+
+    raw doc
+  end
 end
