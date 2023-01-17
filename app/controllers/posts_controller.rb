728d94efb45df0fbbc3c036e993c3c1b14defd58
Dave T <105549233+Dmum303@users.noreply.github.com>
2023-01-17 16:18:50 +0000

    only tweet creator can edit tweet

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to root_path
     else
-      render :new, status: :unprocessable_entity
+      # render :new, status: :unprocessable_entity
     end
   end
 
