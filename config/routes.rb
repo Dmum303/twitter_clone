19618d39d0807115a3f4903ea3775b2e0136152b
Dave T <105549233+Dmum303@users.noreply.github.com>
2023-01-17 11:24:14 +0000

    Issue on merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # root
+  root "posts#index"
 
-  root "sessions#login"
+  #posts and comments route # resources creates all CRUD routes auto
+  resources :posts do
+    resources :comments
+  end
 
-  # User routes
-  resources :users, only: %i[new create edit update show destroy]
-  # get "signup", to: "users#new"
-
-  #Sessions routes
-  get "/login", to: "sessions#login"
-  post "/login", to: "sessions#create"
-  post "/logout", to: "sessions#destroy"
-  get "/logout", to: "sessions#destroy"
+  #signup and sessions
+  get "signup", to: "users#new"
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  resources :users, except: [:new]
 end
