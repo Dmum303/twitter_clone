3e24cb759d13ee81a8db7e41c85d1c7b0fc2d27d
Dave T <105549233+Dmum303@users.noreply.github.com>
2023-01-27 10:07:38 +0000

    All models tested

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
-
+require_relative "../../app/models/user"
+require_relative "../../app/models/post"
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "comment should have a body" do
+    comment = Comment.new(post_id: 1, user_id: 1)
+    assert_not comment.save, "Saved the comment without a body"
+  end
+
+  test "comment body should have at least 5 characters" do
+    comment = Comment.new(post_id: 1, user_id: 1, body: "abc")
+    assert_not comment.save,
+               "Saved the comment with less than 5 characters in the body"
+  end
+
+  test "comment body should have at most 140 characters" do
+    comment = Comment.new(post_id: 1, user_id: 1, body: "a" * 141)
+    assert_not comment.save,
+               "Saved the comment with more than 140 characters in the body"
+  end
+
+  test "comment should belong to a post" do
+    comment = Comment.new(body: "Test comment", user_id: 1)
+    assert_not comment.save, "Saved the comment without a post association"
+  end
+
+  test "comment should belong to a user" do
+    comment = Comment.new(body: "Test comment", post_id: 1)
+    assert_not comment.save, "Saved the comment without a user association"
+  end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "post should have a body" do
+    post = Post.new(user_id: 1)
+    assert_not post.save, "Saved the post without a body"
+  end
+
+  test "post body should have at least 5 characters" do
+    post = Post.new(user_id: 1, body: "abc")
+    assert_not post.save,
+               "Saved the post with less than 5 characters in the body"
+  end
+
+  test "post body should have at most 140 characters" do
+    post = Post.new(user_id: 1, body: "a" * 141)
+    assert_not post.save,
+               "Saved the post with more than 140 characters in the body"
+  end
+
+  test "post should belong to a user" do
+    post = Post.new(body: "Test post")
+    assert_not post.save, "Saved the post without a user association"
+  end
 end
