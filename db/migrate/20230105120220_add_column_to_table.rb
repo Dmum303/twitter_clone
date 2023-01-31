ddf575f77de372b17128e60862d502fad0c3205a
Dave T <105549233+Dmum303@users.noreply.github.com>
2023-01-31 11:01:35 +0000

    switching to postgres

--- db/migrate/20230105120220_add_column_to_table.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnToTable < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :screen_name, :string
-    add_column :users, :real_name, :string
-  end
-end
--- db/migrate/20230117150229_rename_user_columns.rb
+++
@@ -1,7 +0,0 @@
-class RenameUserColumns < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :users, :username, :email
-    rename_column :users, :screen_name, :username
-    rename_column :users, :real_name, :name
-  end
-end
