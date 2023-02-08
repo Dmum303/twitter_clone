2fde46b9a02c025950ee72732c641ea1434e9f78
Dave T <105549233+Dmum303@users.noreply.github.com>
2023-02-08 16:55:02 +0000

    Updating login requirements

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(username: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       flash[:notice] = "Logged in successfully."
