7a1985da570e1c53b5012cc810627bc666989f3e
Dave T <105549233+Dmum303@users.noreply.github.com>
2023-02-08 17:00:58 +0000

    Changing post length req

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ApplicationRecord
 
   belongs_to :user
   validates :body, presence: true
-  validates :body, length: { minimum: 5, maximum: 140 }
+  validates :body, length: { maximum: 240 }
 end
